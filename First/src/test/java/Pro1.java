1a963d7be9adf46e790bd2f4c312a07ef09134e3
Venkata Krishna Chaitanya Doppalapudi <VDoppalapudi@prolifics.com>
2024-05-28 12:33:41 +0530

    First commit

---
+++ First/src/test/java/Pro1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Pro1 {
+
+	public static void main(String[] args) {
+		List<Integer> list1 = new ArrayList();
+		list1.add(1);
+		list1.add(2);
+		System.out.println(list1);
+
+	}
+
+}
